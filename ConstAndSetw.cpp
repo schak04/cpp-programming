c2293a770e85f80a12a144bcbb6212c1428afa32
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2025-10-13 10:14:39 +0530

    Refactor comments to clarify usage of const in C++

--- ConstAndSetw.cpp
+++ ConstAndSetw.cpp
@@ -1,14 +1,20 @@
+/*
+const is used to create read-only variables
+    const -> constants -> can't be changed
+    const float pi = 3.14;
+    -> The value of pi here can't be changed later in the program
+In C++ classes,
+    Use const after the function parentheses to mark the method as not modifying the object.
+    Only use const before the return type if returning a reference or pointer to data you don’t want the caller to change.
+- const after () → protects the object.
+- const before return type → protects the return value.
+*/
+
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
-int main()
-{
-    // const is used to create read-only variables
-    // const -> constants -> can't be changed
-    // const float pi = 3.14;
-    // -> The value of pi here can't be changed later in the program
-
+int main() {
     int a = 10, b = 204, c = 3056;
     cout<<"The value of a without setw is: "<<a<<endl;
     cout<<"The value of b without setw is: "<<b<<endl;
